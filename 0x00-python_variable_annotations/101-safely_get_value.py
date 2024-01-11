f7c282fc6b539ce1d3072e07498b91d09070a7d6
catherinekimani <catherinekimani882@gmail.com>
2024-01-11 23:12:11 +0300

    Type annotation using TypeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Duck typing annotation """
+from typing import TypeVar, Mapping, Union, Any
+
+
+T = TypeVar("T")
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None):
+    """
+    Safely retrieve a value from a dictionary
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
