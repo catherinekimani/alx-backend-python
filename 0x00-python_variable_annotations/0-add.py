cd224a10e1100be9ddde1e224ac2c101cdb02007
catherinekimani <catherinekimani882@gmail.com>
2024-01-11 23:01:57 +0300

    Feat: Add type-annotated funcs for float addition and string concatenation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" type annotation func add """
+
+
+def add(a: float, b: float) -> float:
+    """ Return sum """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" type annotation func concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    returns a concatenated string
+    """
+    return str1 + str2
