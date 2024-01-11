0a7195f7d627108c6d8de9e0f5a9b9e97c45e5e7
catherinekimani <catherinekimani882@gmail.com>
2024-01-11 23:04:11 +0300

    Feat: Type-annotate 'floor' function for float floor calculation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Type annotation func floor """
+import math
+
+
+def floor(n: float) -> float:
+    """
+    returns floor of a given float
+    """
+    return math.floor(n)
