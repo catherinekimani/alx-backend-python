81008646c9b3c5ac76d677c496869e19cec9a4af
catherinekimani <catherinekimani882@gmail.com>
2024-01-15 23:19:50 +0300

    Import wait_random from 0-basic_async_syntax

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,7 +6,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measute total execution time
     """
