46c0fd4b0d86cce18461507ec68bca14cc1975b3
catherinekimani <catherinekimani882@gmail.com>
2024-01-16 23:11:49 +0300

    Add test files to gitignore

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """ Run time for four parallel comprehensions """
     start = time.perf_counter()
     coroutines = []
